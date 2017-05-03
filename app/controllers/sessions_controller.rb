dee400df3d316f76fbbc9eecd63497dc415ff2b5
Joe <joseph_strauss@yahoo.com>
2017-05-03 12:50:49 -0400

    removed pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_omniauth(request.env['omniauth.auth'])
-    binding.pry
     if @user
       session[:user_id] = @user.id
       redirect_to root_path
