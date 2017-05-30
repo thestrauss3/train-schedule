d4b20160fedd912909fa6069c14f09aa1cd169d7
Joe <joseph_strauss@yahoo.com>
2017-05-30 11:35:18 -0400

    remove useless logic in userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,2 @@
-require 'rubygems'
-require 'httparty'
 class UsersController < ApplicationController
-  def show
-    response = HTTParty.get('http://realtime.mbta.com/developer/api/v2/schedulebyroute?api_key=RfQnjyQA7EecUcMOjtbp0Q&route=CR-Worcester&max_trips=100&max_time=1440&datetime=1496116800')
-    binding.pry
-  end
 end
