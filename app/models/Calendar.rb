f5670abaf18daa11e69432bd6069f8fef4ca219b
Joe <joseph_strauss@yahoo.com>
2017-06-02 16:05:22 -0400

    codeship...

--- app/models/Calendar.rb
+++ app/models/Calendar.rb
@@ -1,12 +1,4 @@
 class Calendar < ApplicationRecord
-  validates :service_id,
-            :monday,
-            :tuesday,
-            :wednesday,
-            :thursday,
-            :friday,
-            :saturday,
-            :sunday,
-            :start_date,
-            :end_date, presence: true
+  validates :service_id, :start_date, :end_date, presence: true
+  validates :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday, presence: true
 end
